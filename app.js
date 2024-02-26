1e27deec336fb51daae7a071c852f2de17592cc2
Manuel García <153007701+delafter@users.noreply.github.com>
2024-02-26 20:56:35 +0000

    testCorrecto

---
+++ app.js
@@ -0,0 +1,26 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+const fromDollarToYen = function(valueInEuro) {
+  
+    let valueInYen = valueInEuro * 156.5;
+    
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInEuro) {
+  
+    let valueInPound = valueInEuro * 0.87;
+    
+    return valueInPound;
+}
+
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound }
+
+
+
